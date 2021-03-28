8f131f6a413d6e8fe23b7b696420a0d2b7037601
Haroldmorenoc <haroldmc774@gmail.com>
2021-03-27 23:40:36 -0500

    26-03-21

---
+++ mains.js
@@ -0,0 +1,52 @@
+
+
+
+
+// nombre = "harold";
+// //console.log(nombre);
+// //alert(nombre);
+// document.write(nombre);
+
+
+//let nombre, apellido;
+//const edad = 49;
+//nombre = ("harold");
+//apellido = ("moreno");
+
+//console.log(nombre, apellido, edad);
+
+//string
+let nombre = "Harold Moreno Cortes";
+//number  intenger 
+let edad = 49;
+//para numeros con decimales
+let altura = 1.63 //double  float
+//boleano
+let profesor = false;
+//array son listas
+let hobbies = [nombre, edad, altura, profesor];
+console.log(hobbies);
+console.log(hobbies[2]);
+//tipos de datos objeto object
+let basos = {
+    material: "porcelana",
+    diametro : 60,
+    lleno : false,
+};
+console.log(basos);
+console.log(basos.diametro, basos.material, basos.lleno);
+ 
+// let papa = null;
+// let papa = '';
+// console.log(papa);
+
+
+
+
+//console.log(hobbies);
+//console.log(hobbies[0]);
+
+
+
+
+
