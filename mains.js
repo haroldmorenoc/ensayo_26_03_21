e3fb785f02b55901a834d95005478bfc02c0ae73
Haroldmorenoc <haroldmc774@gmail.com>
2021-03-28 18:03:48 -0500

    26_03_21

--- mains.js
+++ mains.js
@@ -1,32 +1,34 @@
 
+noombre="harold moreno";
+console.log(noombre);
+// // // alert(nombre);
+// // document.write(nombre);
 
 
-
-// nombre = "harold";
-// //console.log(nombre);
-// //alert(nombre);
-// document.write(nombre);
-
-
-//let nombre, apellido;
-//const edad = 49;
-//nombre = ("harold");
-//apellido = ("moreno");
-
+let nombre, apellido;
+const edad = 49;
+nombre = ("Harold");
+apellido = ("Moreno");
+document.write(nombre, apellido, edad);
+//alert(nombre, apellido, edad);
 //console.log(nombre, apellido, edad);
 
 //string
-let nombre = "Harold Moreno Cortes";
+let Nombre = "Harold Moreno Cortes";
 //number  intenger 
-let edad = 49;
+let Edad = 49;
 //para numeros con decimales
-let altura = 1.63 //double  float
+let altura = 1.70 //double  float
 //boleano
-let profesor = false;
+let estudiante = false;
 //array son listas
-let hobbies = [nombre, edad, altura, profesor];
+let hobbies = [Nombre, Edad, altura, estudiante];
+document.write(hobbies);
+document.write(hobbies[2]);
 console.log(hobbies);
 console.log(hobbies[2]);
+
+
 //tipos de datos objeto object
 let basos = {
     material: "porcelana",
@@ -41,10 +43,8 @@ console.log(basos.diametro, basos.material, basos.lleno);
 // console.log(papa);
 
 
-
-
-//console.log(hobbies);
-//console.log(hobbies[0]);
+// console.log(hobbies);
+// console.log(hobbies[0]);
 
 
 
